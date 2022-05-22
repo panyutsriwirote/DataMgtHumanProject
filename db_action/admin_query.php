099ec4e8f2953f0e8155d2cd80cb0773d00b5956
Panyut Sriwirote <mildpanyut@gmail.com>
2022-05-23 02:22:55 +0700

    switch to improving hosted

--- db_action/admin_query.php
+++ db_action/admin_query.php
@@ -8,30 +8,32 @@
   $mode = $_GET["mode"];
   $term = mysqli_real_escape_string($link, $_GET["term"]);
   if ($mode == "std") {
-    $stmt = $link->prepare("SELECT *
+    $stmt = $link->prepare("SELECT std_id AS id, CONCAT(fname_th, ' ', lname_th) AS name
                             FROM student
                             WHERE std_id LIKE CONCAT(?, '%')
                             OR CONCAT(fname_th, ' ', lname_th) LIKE CONCAT('%', ?, '%')
                             OR CONCAT(fname_en, ' ', lname_en) LIKE CONCAT('%', ?, '%')
                             LIMIT 10");
     $stmt->bind_param("sss", $term, $term, $term);
-    $stmt->execute();
-    $result = $stmt->get_result;
   } elseif ($mode == "course") {
-    $stmt = $link->prepare("SELECT *
+    $stmt = $link->prepare("SELECT course_id AS id, course_en_name AS name
                             FROM course
-                            WHERE std_id LIKE CONCAT(?, '%')
-                            OR CONCAT(fname_th, ' ', lname_th) LIKE CONCAT('%', ?, '%')
-                            OR CONCAT(fname_en, ' ', lname_en) LIKE CONCAT('%', ?, '%')
+                            WHERE course_id LIKE CONCAT(?, '%')
+                            OR course_en_name LIKE CONCAT('%', ?, '%')
+                            OR course_th_name LIKE CONCAT('%', ?, '%')
+                            OR course_short_name LIKE CONCAT('%', ?, '%')
                             LIMIT 10");
-    $stmt->bind_param("sss", $term, $term, $term);
-    $stmt->execute();
-    $result = $stmt->get_result;
+    $stmt->bind_param("ssss", $term, $term, $term, $term);
   } else {
     mysqli_close($link);
     exit();
   }
-  $return = [$mode, $term];
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $return = array();
+  while ($row = mysqli_fetch_array($result)) {
+    array_push($return, $row["id"]." ".$row["name"]);
+  }
   echo json_encode($return);
   mysqli_close($link);
 ?>
